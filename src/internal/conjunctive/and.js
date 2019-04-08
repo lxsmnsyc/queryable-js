2cfaa2e71a399771f7c9cf4b051c68602bfe6666
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-08 17:14:04 +0800

    Update and.js

--- src/internal/conjunctive/and.js
+++ src/internal/conjunctive/and.js
@@ -3,7 +3,7 @@ import { isFunction } from '../utils';
 /**
  * Performs a logical conjunction
  */
-export default (...expr) => source => expr.reduce(
-  (acc, x) => acc && (isFunction(x) && x(source)),
+export default (...expr) => row => expr.reduce(
+  (acc, x) => acc && (isFunction(x) && x(row)),
   true,
 );
